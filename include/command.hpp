2b772e6bc8449bf0ad8c89c301773b7785b1fbac
Manas Abhyankar <manasthemenace@gmail.com>
2024-05-07 12:19:13 -0700

    Removed unnecessary command_list.hpp and moved contents permanently to command.hpp

--- include/command.hpp
+++ include/command.hpp
@@ -1,6 +1,6 @@
 using namespace std;
 #include <string>
-
+#include <vector>
 class Command
 {
 private:
@@ -9,3 +9,11 @@ public:
     Command(string command_literal);
     ~Command();
 };
+
+typedef struct CommandList{
+    vector<string> command_list = {
+        "help",
+        "ls",
+        "pwd",
+    };
+} CommandList;
